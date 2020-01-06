b923a60bab4ae0093f755a9150070d5362eb86ae
abhishek.tiwari <abhishek.tiwari@innoeye.com>
2020-01-06 19:04:52 +0530

    thirsty branch created

--- test/test1.js
+++ test/test1.js
@@ -1,8 +1,12 @@
 var a  = "true";
+var b = "thirsty"
 if (a) {
     console.log("Eat Samosa");
     console.log("Eat Pizza");
     console.log("Eat Burger")
 } else {
+    if (b == "thirsty"){
+        console.log ("drink water");
+    }
     console.log("do your homework");
 }

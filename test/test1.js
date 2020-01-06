21d0f71826dbb05f957ca914ad4b3cced219ed37
abhishek.tiwari <abhishek.tiwari@innoeye.com>
2020-01-06 19:12:42 +0530

    fries

--- test/test1.js
+++ test/test1.js
@@ -3,6 +3,7 @@ if (a) {
     console.log("Eat Samosa");
     console.log("Eat Pizza");
     console.log("Eat Burger")
+    console.log("Eat fries")
 } else {
     console.log("do your homework");
 }
